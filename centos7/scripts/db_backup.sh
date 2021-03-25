d9a3e9bb70d6b595b17f57bd794233eaca632fcc
Andrew Grube <andrew.grube@selectquote.com>
2021-03-25 11:53:56 -0500

    Added AWS keys as variables to scripts. Not sure why jenkins ssh won't use the .env file.

--- centos7/scripts/db_backup.sh
+++ centos7/scripts/db_backup.sh
@@ -7,8 +7,12 @@ DB_HOST=$1
 DB_PASSWORD=$2
 DB_NAME=$3
 BUCKET_NAME=$4
+AWS_ACCESS_KEY_ID=$5
+AWS_SECRET_ACCESS_KEY=$6
 
 mysqldump --column-statistics=0 -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /var/run/db/$BACKUP_NAME && \
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID && \
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY && \
 echo "Uploading db backup" && \
 aws s3 cp /var/run/db/$BACKUP_NAME s3://$BUCKET_NAME/$BACKUP_NAME
 
